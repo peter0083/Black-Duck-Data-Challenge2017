62fb05d039405b663043cbce52fca87c65abb1d1
Kai He <sky2010_2050@hotmail.com>
2017-03-10 12:21:38 -0800

    playing around

--- src/explore.R
+++ src/explore.R
@@ -21,8 +21,47 @@ pjt_by_so <- data %>%
   summarise(count = n()) %>% 
   arrange(desc(count))
 
+pjt_by_uuid <- data %>% 
+  group_by(d_r_uuid) %>% 
+  summarise(count = n()) %>% 
+  arrange(desc(count))
 
+pjt_by_so %>% 
+  ggplot(aes(x = count))+
+  geom_histogram()
+
+pjt_by_dns %>% 
+  ggplot(aes(x = count))+
+  geom_histogram()
+
+pjt_by_dws %>% 
+  ggplot(aes(x = count))+
+  geom_histogram()
+
+pjt_by_uuid %>% 
+  ggplot(aes(x = count))+
+  geom_histogram()
+
+pjt_by_lic %>% 
+  ggplot(aes(x = count))+
+  geom_histogram()
 
 data %>% 
-  ggplot(aes(x = license_id))+
-  geom_bar()
+  ggplot(aes(x = license_id))+geom_bar()
+
+top_lic <- left_join(head(pjt_by_lic,100), data)
+
+top_uuid <- data %>% 
+  filter(d_r_uuid %in% pjt_by_uuid$d_r_uuid) %>% 
+
+top_dns <- data %>% 
+  arrange(dns) %>% 
+  head(100)
+
+top_dws <- data %>% 
+  arrange(dws) %>% 
+  head(100)
+
+top_so <- data %>% 
+  arrange(so) %>% 
+  head(100)
